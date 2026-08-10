f3eb6d408beea8f2971105b09092ccc6b0d39a98
Nagamasa Codex <nagamasa-codex@local>
2026-08-10 10:49:29 +0700

    Match live August offer section

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,5 @@
 export const CTA_URL = 'https://zenichat.id/api/wa/52c7314c-e9b8-406e-aa5a-690d5e83afbb/oct-2026';
+export const TICKET_URL = 'https://alphaleaders.myr.id/pl/the-alpha-managers';
 export const CAMPAIGN_ID = 'cfr-oct-2026';
 export const CONTENT_NAME = 'Alpha Managers 3.0 Exclusive Workshop - 1 Oktober 2026';
 export const CONTENT_CATEGORY = 'Alpha Leaders Manager Event';
@@ -22,4 +23,20 @@ export const openRegistrationCTA = () => {
 
   const targetUrl = new URL(CTA_URL);
   window.open(targetUrl.toString(), '_blank', 'noopener,noreferrer');
+};
+
+export const trackTicketCTA = () => {
+  window.fbq?.('trackCustom', 'PaymentCTAClick', {
+    campaign_id: CAMPAIGN_ID,
+    content_name: CONTENT_NAME,
+    content_category: CONTENT_CATEGORY,
+  });
+};
+
+export const trackWhatsAppCTA = () => {
+  window.fbq?.('trackCustom', 'WhatsAppCTAClick', {
+    campaign_id: CAMPAIGN_ID,
+    content_name: CONTENT_NAME,
+    content_category: CONTENT_CATEGORY,
+  });
 };
